90b2756933367aaf6e65f4d6c117ccea3a7bbc83
mk <info@markokaestner.com>
2013-04-18 13:52:27 +0200

    #18 Query database for project search to improve performance

--- workflows/OF-TaskActions/src/findProject.sh
+++ workflows/OF-TaskActions/src/findProject.sh
@@ -3,31 +3,32 @@
 QUERY=$1
 
 OFOC="com.omnigroup.OmniFocus"
-if [ ! -d "$HOME/Library/Caches/Metadata/$OFOC" ]; then OFOC=$OFOC.MacAppStore; fi
+if [ ! -d "$HOME/Library/Caches/$OFOC" ]; then OFOC=$OFOC.MacAppStore; fi
+
+SQL="SELECT p.pk, t.name, p.status, p.numberOfAvailableTasks, p.numberOfRemainingTasks, p.containsSingletonActions, f.name FROM (ProjectInfo p LEFT JOIN Task t ON p.task=t.persistentIdentifier) LEFT JOIN Folder f ON p.folder=f.persistentIdentifier WHERE p.status = 'active' AND lower(t.name) LIKE lower('%${QUERY}%') ORDER BY p.containsSingletonActions DESC, t.name ASC"
 
 OLDIFS=$IFS
 IFS='
 '
-PROJECTS=($(mdfind -onlyin ~/Library/Caches/Metadata/${OFOC} "kMDItemTitle == '*${QUERY}*'c"))
+PROJECTS=$(sqlite3 ${HOME}/Library/Caches/${OFOC}/OmniFocusDatabase2 "${SQL}")
 
 echo "<?xml version='1.0'?><items>"
 
 for P in ${PROJECTS[*]}; do
-  P_DATA=$(mdls ${P})
-  PNAME=$(echo "$P_DATA" |grep "kMDItemTitle")
-  PNAME=${PNAME##*= \"}
-  PNAME=${PNAME%%\"*}
-  PFOLDER=$(echo "$P_DATA" |grep "com_omnigroup_OmniFocus_DirectFolderName")
-  PFOLDER=${PFOLDER##*= \"}
-  PFOLDER=${PFOLDER%%\"*}
-  PSTATUS=$(echo "$P_DATA" |grep "com_omnigroup_OmniFocus_Status")
-  PSTATUS=${PSTATUS##*= \"}
-  PSTATUS=${PSTATUS%%\"*}
-  TASKCOUNT=$(echo "$P_DATA" |grep "com_omnigroup_OmniFocus_TaskCount")
-
-  echo "<item uid='ofproject' arg='${PNAME}'><title>${PNAME} (${PFOLDER})</title><subtitle>Status: ${PSTATUS}  |  Tasks: ${TASKCOUNT##*= }</subtitle><icon>img/project.png</icon></item>"
+  PID=${P%%|*}
+  REST=${P#*|}
+  PNAME=${REST%%|*}
+  REST=${REST#*|}
+  PSTATUS=${REST%%|*}
+  REST=${REST#*|}
+  PAVAILABLE=${REST%%|*}
+  REST=${REST#*|}
+  PREMAINING=${REST%%|*}
+  REST=${REST#*|}
+  PSINGLE=${REST%%|*}
+  PFOLDER=${REST##*|}
+  echo "<item uid='${PID}' arg='${PNAME}'><title>${PNAME} (${PFOLDER})</title><subtitle>Status: ${PSTATUS}  |  Available Tasks: ${PAVAILABLE}</subtitle><icon>img/project${PSINGLE}.png</icon></item>"
 done
-
 echo "</items>"
 
 IFS=$OLDIFS
